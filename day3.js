a4d78b9dbf3a397055f58d1c057787cee86c0df9
DeepChak <127760974+DeepChak@users.noreply.github.com>
2023-03-22 20:06:30 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,47 @@
+let myResume = {
+    PersonalData: {
+        Name : "Deepan Chakkaravarthy P Y",
+        Email: "deepanyogeeshwaran@gmail.com",
+        PhoneNumber: 9677455800,
+        Address:{
+            DoorNo:155,
+            Area:"Krishnapuram Colony",
+            City:"Madurai",
+            Pincode: 625014,
+        },
+    },
+    Education:{
+        SSLC:{
+            "University" : "Balamandiram higher Secondry School",
+            "Year of passing":2013,
+            "Percentage": '75%',
+        },
+        HSC:{
+           "University": "Thiyagarajar Model Higher School",
+           "Year of passing": 2015,
+           "Percentage": '78%',
+        },
+        UnderGraduate:{
+            "University": "Madurai Kamarajar University",
+            "Year of passing": 2018,
+            "Percentage": '59.8%',
+        },
+        PostGraduate:{
+            "University": "Madurai Kamarajar University",
+            "Year of passing": 2020,
+            "Percentage": '58.8%',
+        },
+    },
+    Experience:{
+        ProcessExecutive:{
+            Company:"Infoys BPM Ltd",
+            Year:"from 2021 to Till",
+        },
+    },
+    Skills:[
+        "java script",
+        "Hard Worker",
+        "Good Communication Skills",
+    ],
+}
+console.log(myResume);
